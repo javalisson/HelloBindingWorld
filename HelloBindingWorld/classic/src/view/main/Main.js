11a4a3771b88a2b5890926ae68b178080a15a07a
JavAlisson <javalisson@gmail.com>
2017-05-24 20:41:58 -0300

    Removidos arquivos gerados automaticamente e modificadas as views para exibir uma mensagem de Hello World.

--- HelloBindingWorld/classic/src/view/main/Main.js
+++ HelloBindingWorld/classic/src/view/main/Main.js
@@ -6,7 +6,7 @@
  * TODO - Replace this content of this view to suite the needs of your application.
  */
 Ext.define('HelloBindingWorld.view.main.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.panel.Panel',
     xtype: 'app-main',
 
     requires: [
@@ -14,8 +14,7 @@ Ext.define('HelloBindingWorld.view.main.Main', {
         'Ext.window.MessageBox',
 
         'HelloBindingWorld.view.main.MainController',
-        'HelloBindingWorld.view.main.MainModel',
-        'HelloBindingWorld.view.main.List'
+        'HelloBindingWorld.view.main.MainModel'
     ],
 
     controller: 'main',
@@ -23,82 +22,10 @@ Ext.define('HelloBindingWorld.view.main.Main', {
 
     ui: 'navigation',
 
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
-
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
-    },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            plugins: 'responsive',
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
-    },
+    title: 'Hello, Binding World!',
 
     items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{
-            xtype: 'mainlist'
-        }]
-    }, {
-        title: 'Users',
-        iconCls: 'fa-user',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
+        xtype: 'container',
+        html: 'Hello, Binding World!'
     }]
 });
--- HelloBindingWorld/modern/src/view/main/Main.js
+++ HelloBindingWorld/modern/src/view/main/Main.js
@@ -6,56 +6,25 @@
  * TODO - Replace the content of this view to suit the needs of your application.
  */
 Ext.define('HelloBindingWorld.view.main.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.Panel',
     xtype: 'app-main',
 
     requires: [
         'Ext.MessageBox',
 
         'HelloBindingWorld.view.main.MainController',
-        'HelloBindingWorld.view.main.MainModel',
-        'HelloBindingWorld.view.main.List'
+        'HelloBindingWorld.view.main.MainModel'
     ],
 
     controller: 'main',
     viewModel: 'main',
 
-    defaults: {
-        tab: {
-            iconAlign: 'top'
-        },
-        styleHtmlContent: true
-    },
-
-    tabBarPosition: 'bottom',
+    title: 'Hello, Binding World!',
 
     items: [
         {
-            title: 'Home',
-            iconCls: 'x-fa fa-home',
-            layout: 'fit',
-            // The following grid shares a store with the classic version's grid as well!
-            items: [{
-                xtype: 'mainlist'
-            }]
-        },{
-            title: 'Users',
-            iconCls: 'x-fa fa-user',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Groups',
-            iconCls: 'x-fa fa-users',
-            bind: {
-                html: '{loremIpsum}'
-            }
-        },{
-            title: 'Settings',
-            iconCls: 'x-fa fa-cog',
-            bind: {
-                html: '{loremIpsum}'
-            }
+            xtype: 'container',
+            html: 'Hello, Binding World!'
         }
     ]
 });
